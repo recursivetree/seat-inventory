2975eac4e72ba138be7a312dcae9732ff9f8f8aa
recursive_tree <recursivetreemail@gmail.com>
2021-12-26 12:53:00 +0100

    fixed a bug

--- src/Observers/FittingPluginFittingObserver.php
+++ src/Observers/FittingPluginFittingObserver.php
@@ -4,6 +4,7 @@ namespace RecursiveTree\Seat\TerminusInventory\Observers;
 
 use Exception;
 use Illuminate\Support\Facades\DB;
+use RecursiveTree\Seat\TerminusInventory\Helpers\ItemHelper;
 use RecursiveTree\Seat\TerminusInventory\Helpers\Parser;
 use RecursiveTree\Seat\TerminusInventory\Models\Stock;
 use RecursiveTree\Seat\TerminusInventory\Models\StockItem;
@@ -11,27 +12,40 @@ use RecursiveTree\Seat\TerminusInventory\Models\StockItem;
 class FittingPluginFittingObserver
 {
     public function saved($fitting){
-        $stock = Stock::where("fitting_plugin_fitting_id",$fitting->id)->first();
+        //search for linked stocks
+        $stocks = Stock::where("fitting_plugin_fitting_id",$fitting->id)->get();
 
-        if($stock!=null) {
+        //change every stock
+        foreach ($stocks as $stock) {
+            //parse the fit
             try {
                 $fit = Parser::parseFit($fitting->eftfitting);
             } catch (Exception $e){
+                //if parsing fails, use the name of the stock as a way to display the error
                 $m = $e->getMessage();
                 $stock->name = "[Out Of Sync] $stock->name | Error: $m";
                 $stock->save();
                 return;
             }
 
-            DB::transaction(function () use ($fit, $stock) {
+            //after parsing the fit, simplify it
+            $items = ItemHelper::simplifyItemList($fit["items"]);
+
+            //change the db
+            DB::transaction(function () use ($fit, $stock, $items) {
+
+                // update the name
                 $stock->name = $fit["name"];
                 $stock->save();
 
+                //get the id to link items
                 $id=$stock->id;
 
+                //first, remove all old items
                 StockItem::where("stock_id",$id)->delete();
 
-                foreach ($fit["items"] as $item){
+                //inset new items
+                foreach ($items as $item){
                     $item->stock_id = $id;
                     $item->save();
                 }
