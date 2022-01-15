1252cd9899e95f6a4142c2826d10d3e59b4a795b
recursive_tree <recursivetreemail@gmail.com>
2022-01-15 20:37:48 +0100

    fixed migrations

--- src/database/migrations/2021_12_12_000001_AddTables.php
+++ src/database/migrations/2021_12_12_000001_AddTables.php
@@ -14,49 +14,60 @@ class AddTables extends Migration
 {
     public function up()
     {
-        //return;
-        Schema::create('recursive_tree_seat_inventory_tracked_corporations', function (Blueprint $table) {
-            $table->bigInteger("corporation_id")->unsigned();
-            $table->bigIncrements("id");
-        });
+        if (!Schema::hasTable('recursive_tree_seat_inventory_tracked_corporations')) {
+            Schema::create('recursive_tree_seat_inventory_tracked_corporations', function (Blueprint $table) {
+                $table->bigInteger("corporation_id")->unsigned();
+                $table->bigIncrements("id");
+            });
+        }
 
-        Schema::create('recursive_tree_seat_inventory_stock_definitions', function (Blueprint $table) {
-            $table->bigIncrements("id");                                //id
-            $table->bigInteger("location_id");                          //location of the item
-            $table->string("name");                                     //name of the stock definition(fit name or something else)
-            $table->bigInteger("fitting_plugin_fitting_id")->nullable();//used to reload the data from the fitting plugin
-            $table->integer("amount");                                  //how many multiples of this stock definition should be kept in stock
-            $table->boolean("check_contracts");                         //should we check contracts for this stock definition
-            $table->boolean("check_corporation_hangars");               //should we check corporation hangars for this stock definition
-        });
+        if (!Schema::hasTable('recursive_tree_seat_inventory_stock_definitions')) {
+            Schema::create('recursive_tree_seat_inventory_stock_definitions', function (Blueprint $table) {
+                $table->bigIncrements("id");                                //id
+                $table->bigInteger("location_id");                          //location of the item
+                $table->string("name");                                     //name of the stock definition(fit name or something else)
+                $table->bigInteger("fitting_plugin_fitting_id")->nullable();//used to reload the data from the fitting plugin
+                $table->integer("amount");                                  //how many multiples of this stock definition should be kept in stock
+                $table->boolean("check_contracts");                         //should we check contracts for this stock definition
+                $table->boolean("check_corporation_hangars");               //should we check corporation hangars for this stock definition
+            });
+        }
 
-        Schema::create('recursive_tree_seat_inventory_stock_items', function (Blueprint $table) {
-            $table->bigIncrements("id");
-            $table->bigInteger("stock_id");
-            $table->bigInteger("type_id");
-            $table->bigInteger("amount")->unsigned();
-        });
+        if (!Schema::hasTable('recursive_tree_seat_inventory_stock_items')) {
+            Schema::create('recursive_tree_seat_inventory_stock_items', function (Blueprint $table) {
+                $table->bigIncrements("id");
+                $table->bigInteger("stock_id");
+                $table->bigInteger("type_id");
+                $table->bigInteger("amount")->unsigned();
+            });
+        }
 
-        Schema::create('recursive_tree_seat_inventory_inventory_source', function (Blueprint $table) {
-            $table->bigIncrements("id");
-            $table->bigInteger("location_id")->nullable();                  //location of the item
-            $table->string("source_name");                                  //the name of this inventory source, e.g. a corporation hangar or contract
-            $table->enum('source_type', ['corporation_hangar', 'contract']);//type of source
-        });
+        if (!Schema::hasTable('recursive_tree_seat_inventory_inventory_source')) {
+            Schema::create('recursive_tree_seat_inventory_inventory_source', function (Blueprint $table) {
+                $table->bigIncrements("id");
+                $table->bigInteger("location_id")->nullable();                  //location of the item
+                $table->string("source_name");                                  //the name of this inventory source, e.g. a corporation hangar or contract
+                $table->enum('source_type', ['corporation_hangar', 'contract']);//type of source
+            });
+        }
 
-        Schema::create('recursive_tree_seat_inventory_inventory_item', function (Blueprint $table) {
-            $table->bigIncrements("id");
-            $table->bigInteger("source_id");
-            $table->bigInteger("type_id");
-            $table->bigInteger("amount")->unsigned();
-        });
+        if (!Schema::hasTable('recursive_tree_seat_inventory_inventory_item')) {
+            Schema::create('recursive_tree_seat_inventory_inventory_item', function (Blueprint $table) {
+                $table->bigIncrements("id");
+                $table->bigInteger("source_id");
+                $table->bigInteger("type_id");
+                $table->bigInteger("amount")->unsigned();
+            });
+        }
 
-        Schema::create('recursive_tree_seat_inventory_locations', function (Blueprint $table){
-            $table->bigIncrements("id");
-            $table->bigInteger("structure_id")->nullable();                 //citadel where the item is stored
-            $table->bigInteger("station_id")->nullable();                   //npc station of the item
-            $table->string("name");
-        });
+        if (!Schema::hasTable('recursive_tree_seat_inventory_locations')) {
+            Schema::create('recursive_tree_seat_inventory_locations', function (Blueprint $table) {
+                $table->bigIncrements("id");
+                $table->bigInteger("structure_id")->nullable();                 //citadel where the item is stored
+                $table->bigInteger("station_id")->nullable();                   //npc station of the item
+                $table->string("name");
+            });
+        }
 
         $stations = UniverseStation::all();
         $observer = new UniverseStationObserver();
