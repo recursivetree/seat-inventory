ecf27b42179997c878a89021dc77609b62d82b3a
recursive_tree <recursivetreemail@gmail.com>
2022-03-13 15:39:39 +0100

    readded ids to tables

---
+++ src/database/migrations/2022_03_09_000007_FixOptimize.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+use RecursiveTree\Seat\Inventory\Jobs\UpdateInventory;
+
+class FixOptimize extends Migration
+{
+    public function up()
+    {
+        Schema::table('recursive_tree_seat_inventory_inventory_item', function (Blueprint $table) {
+            $table->bigIncrements("id");
+        });
+
+        Schema::table('recursive_tree_seat_inventory_stock_items', function (Blueprint $table) {
+            $table->bigIncrements("id");
+        });
+
+        UpdateInventory::dispatch()->onQueue('default');
+    }
+
+    public function down()
+    {
+        Schema::table('recursive_tree_seat_inventory_inventory_item', function (Blueprint $table) {
+            $table->dropColumn("id");
+        });
+
+        Schema::table('recursive_tree_seat_inventory_stock_items', function (Blueprint $table) {
+            $table->dropColumn("id");
+        });
+    }
+}
+
