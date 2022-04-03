91c1aa3f9c25955e41f1e4570731c414203b50a3
recursive_tree <recursivetreemail@gmail.com>
2022-04-03 17:55:03 +0200

    eveappraisal link

--- src/resources/views/includes/multibuy.blade.php
+++ src/resources/views/includes/multibuy.blade.php
@@ -5,19 +5,27 @@
 <div class="modal fade text-muted" id="multibuyModal{{$id}}" tabindex="-1" style="font-size: 1rem;">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Multibuy</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <textarea class="w-100" rows="15" id="multibuyTextArea{{$id}}" onclick="this.focus();this.select();document.execCommand('copy');" readonly="readonly">{{ $multibuy }}</textarea>
-            </div>
-            <div class="modal-footer">
-                <button id="multibuyCopyButton{{$id}}" class="btn btn-primary">Copy</button>
-                <button class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
+            <form method="POST" action="https://evepraisal.com/appraisal" target="_blank">
+                <input type="hidden" value="no" name="persist">
+                <input type="hidden" value="100" name="price_percentage">
+                <input type="hidden" value="24h" name="expire_after">
+                <input type="hidden" value="jita" name="market">
+
+                <div class="modal-header">
+                    <h5 class="modal-title">Multibuy</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <textarea name="raw_textarea" class="w-100" rows="15" id="multibuyTextArea{{$id}}" onclick="this.focus();this.select();document.execCommand('copy');" readonly="readonly">{{ $multibuy }}</textarea>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" id="multibuyCopyButton{{$id}}" class="btn btn-primary">Copy</button>
+                    <button type="submit" class="btn btn-secondary">EveAppraisal</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
