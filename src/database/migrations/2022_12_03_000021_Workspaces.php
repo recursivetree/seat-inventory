bead78e660ade62087a863106210c9bb36be5eab
recursive_tree <recursivetreemail@gmail.com>
2022-12-06 16:51:47 +0100

    add workspace id to inventory source

--- src/database/migrations/2022_12_03_000021_Workspaces.php
+++ src/database/migrations/2022_12_03_000021_Workspaces.php
@@ -14,7 +14,7 @@ class Workspaces extends Migration
             $table->bigIncrements("id");
             $table->string("name");
         });
-
+//
         $default = new \RecursiveTree\Seat\Inventory\Models\Workspace();
         $default->name = "Default Workspace";
         $default->save();
@@ -33,6 +33,14 @@ class Workspaces extends Migration
         });
         DB::statement("UPDATE seat_inventory_tracked_alliances SET workspace_id = $default_workspace_id");
 
+        Schema::rename('recursive_tree_seat_inventory_inventory_source','seat_inventory_inventory_source');
+
+        Schema::table('seat_inventory_inventory_source', function (Blueprint $table) {
+            $table->bigInteger("workspace_id");
+            $table->index("workspace_id");
+            $table->index("location_id");
+        });
+        DB::statement("UPDATE seat_inventory_inventory_source SET workspace_id = $default_workspace_id");
     }
 
     public function down()
@@ -42,6 +50,10 @@ class Workspaces extends Migration
             $table->dropColumn("workspace_id");
         });
         Schema::table('seat_inventory_tracked_alliances', function (Blueprint $table) {
+            $table->dropColumn("workspace_id");
+        });
+        Schema::rename('seat_inventory_inventory_source','recursive_tree_seat_inventory_inventory_source');
+        Schema::table('recursive_tree_seat_inventory_inventory_source', function (Blueprint $table) {
             $table->dropColumn("workspace_id");
         });
     }
